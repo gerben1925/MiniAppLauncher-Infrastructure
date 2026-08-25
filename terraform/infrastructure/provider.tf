69298ce185261b8089ac7f1ed88198039120a031
Gerben <gerbenclapero@gmail.com>
2026-08-25 20:55:36 +0800

    modified the provider.tf

--- terraform/infrastructure/provider.tf
+++ terraform/infrastructure/provider.tf
@@ -1,5 +1,6 @@
 provider "proxmox" {
-  endpoint  = var.proxmox_api_url
+#   endpoint  = var.proxmox_api_url
+  endpoint  = "https://192.168.0.82:8006/"
   api_token = var.proxmox_api_token
   insecure  = true
 
